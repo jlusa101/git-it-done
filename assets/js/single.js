5eded2d6d1677e8e7e77086d9725a9b6fd488038
Joona Lusa <joona.lusa@hotmail.com>
2021-11-21 20:09:00 -0500

    able to dynamically create a list of issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,17 @@
 var issuesContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+}
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
     fetch(apiUrl).then(function(response) {
@@ -16,7 +27,8 @@ var getRepoIssues = function(repo) {
                 }
             })
         } else {
-            alert("There was a problem with your request!");
+            // If not successfull, redirect back to home page
+            document.location.replace("./index.html");
         }
     })
 };
@@ -72,4 +84,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 }
 
-getRepoIssues("facebook/react");
+getRepoName();
